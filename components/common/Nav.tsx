6d6025d2154a04f57b721b11a6f0dcd7ae5f4b78
Koigor97 <turaysamuel101@yahoo.com>
2024-10-06 01:12:16 +0200

    complete responsive nav

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -1,17 +1,14 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useMediaQuery } from "@uidotdev/usehooks";
 import clsx from "clsx";
+import { AlignRight, XIcon } from "lucide-react";
+import { motion } from "framer-motion";
 
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-} from "../ui/dropdown-menu";
-import { AlignRight } from "lucide-react";
+import { Button } from "../ui/button";
 
 const links = [
   { href: "/", label: "Home" },
@@ -19,67 +16,92 @@ const links = [
   { href: "/blog", label: "Dev Blog" },
 ];
 
-function Nav() {
-  const pathname = usePathname();
-
-  return (
-    <nav>
-      <ul className="text-lg text-bodyText/85 font-medium flex gap-5">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={clsx({
-                "text-secondary1 font-bold underline underline-offset-4":
-                  pathname === link.href,
-              })}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-}
-
-function MobileNav() {
-  const pathname = usePathname();
-
-  return (
-    <nav>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <AlignRight size={34} className="py-1 px-1" />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {links.map((link) => (
-            <DropdownMenuItem
-              key={link.href}
-              className={clsx(
-                {
-                  "bg-secondary1 font-bold text-bgBody": pathname === link.href,
-                },
-                "text-base"
-              )}
-            >
-              <Link href={link.href}>{link.label}</Link>
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </nav>
-  );
-}
-
 function ResponsiveNav() {
-  const isMobile = useMediaQuery("(max-width: 768px)");
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
-  if (isMobile) {
-    return <MobileNav />;
+  function handleToggle() {
+    setIsOpen((prev) => !prev);
   }
 
-  return <Nav />;
+  // check window size on load and window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // initial check
+    handleResize();
+
+    //  listen for window resize
+    window.addEventListener("resize", handleResize);
+
+    // clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <nav>
+      <div className="flex justify-between items-center py-4">
+        <AlignRight className="w-7 h-7 md:hidden" onClick={handleToggle} />
+
+        <ul
+          className={`${
+            isMobile ? "hidden" : "flex"
+          } text-lg text-bodyText/85 font-medium gap-5`}
+        >
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={clsx({
+                  "text-secondary1 font-bold underline underline-offset-4":
+                    pathname === link.href,
+                })}
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={isOpen ? { x: 0 } : { x: "100%" }}
+        transition={{ duration: 0.5 }}
+        className="fixed top-0 right-0 w-1/2 h-screen bg-white z-50 lg:hidden py-3"
+      >
+        <div className="grid gap-5">
+          <XIcon onClick={handleToggle} className="ml-2" />
+          <ul className="px-2">
+            {links.map((link) => (
+              <li
+                key={link.href}
+                className={clsx(
+                  {
+                    "bg-secondary1 text-bgBody font-bold ":
+                      pathname === link.href,
+                  },
+                  "px-4 py-2 rounded-md"
+                )}
+              >
+                <Link
+                  href={link.href}
+                  onClick={handleToggle}
+                  className="text-lg"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </motion.div>
+    </nav>
+  );
 }
 
 export default ResponsiveNav;
