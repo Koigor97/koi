9ee3f073bd49f80c82f5af892357cf924322f62a
Koigor97 <turaysamuel101@yahoo.com>
2024-10-06 15:53:38 +0200

    add about section, Experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/components/layout/HeroSection";
+import AboutSection from "@/components/layout/AboutSection";
+import ExperienceAndSkillsSection from "@/components/layout/ExperienceAndSkillsSection";
 
 export default function Home() {
   return (
-    <div>
+    <div className="grid gap-7">
       <HeroSection />
+      <AboutSection />
+      <ExperienceAndSkillsSection />
     </div>
   );
 }
