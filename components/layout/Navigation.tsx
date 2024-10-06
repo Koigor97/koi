9e30ed827cce0fdf1cc7f1282942e835a3788e54
Koigor97 <turaysamuel101@yahoo.com>
2024-10-06 04:02:28 +0200

    remove max-width css styling

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -3,7 +3,7 @@ import ResponsiveNav from "../common/Nav";
 
 function Navigation() {
   return (
-    <div className="flex items-center justify-between pt-2 max-w-custom-clamp-3 mx-auto md:max-w-custom-clamp-2 ">
+    <div className="flex items-center justify-between pt-2 mb-3 md:mb-5">
       <Logo className="w-12 md:w-14" />
       <ResponsiveNav />
     </div>
